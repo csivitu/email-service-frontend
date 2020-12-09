cf00e63c4bf87f04dab023ce3d8c047018db27f5
alias-rahil <rahil.kabani.4@gmail.com>
2020-12-10 01:26:04 +0530

    feat: added basic layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import './HomePage.css';
 
 function Home() {
+    const state = { submitted: '' };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        state.submitted = 'done!';
+        event.target.reset();
+    };
     return (
-        <h1>Hello, world!</h1>
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    SUBJECT:
+                    <input name="subject" type="text" />
+                </label>
+                <br />
+                <br />
+                <label>
+                    HTML:
+                    <textarea name="html" />
+                </label>
+                <br />
+                <br />
+                <label>
+                    TEXT (alternate text that will only be displayed if html is not supported):
+                    <textarea name="text" />
+                </label>
+                <br />
+                <br />
+                <label>
+                    AUTHKEY
+                    <input name="auth" type="password" required />
+                </label>
+                <br />
+                <br />
+                <label>
+                    TO (upload txt file):
+                    <input name="to" type="file" accept="text/plain" required />
+                </label>
+                <br />
+                <br />
+                <input type="submit" value="SEND" />
+            </form>
+            <p>{state.submitted}</p>
+        </div>
     );
 }
 
